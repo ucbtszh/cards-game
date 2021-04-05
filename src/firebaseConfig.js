4b5096fc233217eb27cdfe66605ac0970f07a492
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-04-05 14:51:35 +0200

    installed firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,45 @@
+import firebase from "firebase/app";
+import "./fireabse/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAI6GeYj-5UFAFbs6ckdeWV622vpTPVR-E",
+    authDomain: "cards-dd-game.firebaseapp.com",
+    projectId: "cards-dd-game",
+    storageBucket: "cards-dd-game.appspot.com",
+    messagingSenderId: "349803451655",
+    appId: "1:349803451655:web:4a4bb416a26f8d97f421a2"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const subjectsCollection = db.collection("subjects");
+
+function writeMetaUser(uid, data) {
+      subjectsCollection
+        .doc(uid)
+        .set(data)
+        // .then(() => {
+        //   console.log("New user collection successfully created.");
+        // })
+        .catch((error) => {
+          console.error("Error creating document: ", error);
+        });
+    }
+
+function writeResponseData(uid, responseName, data) {
+      let newPath = subjectsCollection
+        .doc(uid)
+        .collection("responses")
+        .doc(responseName);
+      newPath
+        .set(data)
+        // .then(() => {
+        //   console.log(responseName, "data successfully written.");
+        // })
+        .catch((error) => {
+          console.error("Error writing", responseName, " data: ", error);
+        });
+    }
+
+export { writeMetaUser, writeResponseData }
--- src/mixin/meta.js
+++ src/mixin/meta.js
@@ -1,39 +1,45 @@
-// TODO: PUSH ALL THIS TO DB
-
 import platform from 'platform'
+import store from './firestore_utils'
 
 export default {
     mounted() {
         // screen resolution
         var w = window.screen.width
         var h = window.screen.height
-        console.log("screen resolution", w, h)
 
         // is touchscreen device?
         var hasTouchScreen = (
             /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
             /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
             );
-        console.log("has touchscreen", hasTouchScreen)
         
         // OS
         var OSName = platform.os.family
         var OSArch = platform.os.architecture
-        console.log("OS", OSName, OSArch)
         
         // browser
-        var UA = navigator.userAgent;
-        console.log("browser", UA)
+        var UA = navigator.subjectAgent;
         
         // timezone
         var d = new Date();
         var n = d.getTimezoneOffset();
-        console.log("local timezone", n)
         
         // preferred browser language
         var lang = navigator.languages
         ? navigator.languages[0]
-        : (navigator.language || navigator.userLanguage)
-        console.log("preferred browser language", lang)
+        : (navigator.language || navigator.subjectLanguage)
+
+        let metaInfo = {
+            starttime: Date.now(),
+            screen_width: w,
+            screen_height: h,
+            touchscreen: hasTouchScreen,
+            os_name: OSName,
+            os_arch: OSArch,
+            browser: UA,
+            min_to_utc: n,
+            language: lang
+        }
+        store.methods.writeMetaSubject(this.$uuid, metaInfo)
     }
 }
