6d6bc0d018910b314181c5761111cfd09b77b916
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-04-07 20:36:09 +0200

    added completion code link

--- src/views/End.vue
+++ src/views/End.vue
@@ -2,9 +2,10 @@
     <div id="end">
         <b>Debriefing</b><br>
         In this game, you played against a computer, not another actual player. This was necessary for our research purposes. <br><br>
-        Thank you very much for participating in this study! We greatly appreciate your time and effort in doing so, as it allows us to continue academic research during these challenging times.<br><br>
-        Your participation will be logged automatically, once you click the below button:<br /><br />
-        <v-btn href="" color="primary" elevation="3"
+        Thank you very much for participating in this study! <br />
+        We greatly appreciate your time and effort in doing so, as it allows us to continue academic research during these challenging times.<br><br>
+        Your participation will be logged automatically once you click the below button:<br /><br />
+        <v-btn href="https://app.prolific.co/submissions/complete?cc=7BF55447" color="primary" elevation="3"
         ><b>Take me back to Prolific and log my participation</b>
         </v-btn>
     </div>
@@ -12,8 +13,6 @@
 
 <script>
 import { writeResponseData } from "../firebaseConfig"
-
-// TODO: ADD COMPLETION CODE URL FROM PROLIFIC
 
 export default {
     methods: {
