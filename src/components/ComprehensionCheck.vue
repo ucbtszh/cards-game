ed7cb26e23e1e3cef9192509c73e895e756add46
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-05-27 20:51:18 +0200

    redirect at two failed comprehension check attempts

--- src/components/ComprehensionCheck.vue
+++ src/components/ComprehensionCheck.vue
@@ -1,159 +1,193 @@
 <template>
-        <div id="comprehension">
-                <div id="c1" v-show="showC1">
-                <v-form v-model="isvalid">
-                        In each trial, you and the other player will be presented with an identical set of five cards.<br>
-                        <v-radio-group
-                        :rules="[(v) => !!v || 'Please answer this question.']" v-model="c1">
-                                <v-radio
-                                value="1"
-                                label="True"
-                                @click="c1clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="0"
-                                label="False"
-                                @click="c1clicks++"
-                                >
-                                </v-radio>
-                        </v-radio-group>
-                        <v-btn color="success" elevation="3" :disabled="!isvalid" @click="validateC1()">
-                                Submit
-                        </v-btn>
-                </v-form>
-                </div>
-                <div id="c2" v-show="showC2">
-                <v-form v-model="isvalid2">
-                        The computer picks one card for you and the other player at random. Which of the below statements is true?<br>
-                        <v-radio-group
-                        :rules="[(v) => !!v || 'Please answer this question.']" v-model="c2">
-                                <v-radio
-                                value="a"
-                                label="The computer picks the same card for you and the other player"
-                                @click="c2clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="b"
-                                label="The computer picks a different card for you and the other player."
-                                @click="c2clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="c"
-                                label="The computer makes two random picks: one for you and one for the other player. This card can be the same or a different card."
-                                @click="c2clicks++"
-                                >
-                                </v-radio>
-                        </v-radio-group>
-                        <v-btn color="success" elevation="3" :disabled="!isvalid2" @click="validateC2()">
-                                Submit
-                        </v-btn>
-                </v-form>
-                </div>
-                <div id="c3" v-show="showC3">
-                <v-form v-model="isvalid3">
-                        Let’s say you reported the blue card and the other player reported red:<br>
-                        <v-radio-group
-                        :rules="[(v) => !!v || 'Please answer this question.']" v-model="c3">
-                                <v-radio
-                                value="a"
-                                label="You win 1 point. The other player wins nothing."
-                                @click="c3clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="b"
-                                label="The other player wins 1 point.  You win nothing."
-                                @click="c3clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="c"
-                                label="You win 1 point. The other player loses 1 point."
-                                @click="c3clicks++"
-                                >
-                                </v-radio>
-                                <v-radio
-                                value="d"
-                                label="The other player wins 1 point.  You lose 1 point."
-                                @click="c3clicks++"
-                                >
-                                </v-radio>
-                        </v-radio-group>
-                        <v-btn color="success" elevation="3" :disabled="!isvalid3" @click="validateC3()">
-                                Submit
-                        </v-btn>
-                </v-form>
-                </div>
-                <div id="c4" v-show="showC4">
-        Well done!<br><br>
-        Next, you will play three <b>practice trials</b>.<br>
-        After these, the actual game will start. <br><br>
-        <v-btn color="primary" elevation="3" @click=" saveAttempts(); $router.push('testtrials');">Continue</v-btn>
-                </div>
+  <div id="comprehension">
+    <div id="c1" v-show="showC1">
+      <v-form v-model="isvalid">
+        In each trial, you and the other player will be presented with an
+        identical set of five cards.<br />
+        <v-radio-group
+          :rules="[(v) => !!v || 'Please answer this question.']"
+          v-model="c1"
+        >
+          <v-radio value="1" label="True"></v-radio>
+          <v-radio value="0" label="False"></v-radio>
+        </v-radio-group>
+        <v-btn
+          color="success"
+          elevation="3"
+          :disabled="!isvalid"
+          @click="
+            c1clicks++;
+            validateC1();
+          "
+        >
+          Submit
+        </v-btn>
+        <div v-show="c1clicks == 1" style="color:red">
+          {{ warning }}
         </div>
+      </v-form>
+    </div>
+    <div id="c2" v-show="showC2">
+      <v-form v-model="isvalid2">
+        The computer picks one card for you and the other player at random.
+        Which of the below statements is true?<br />
+        <v-radio-group
+          :rules="[(v) => !!v || 'Please answer this question.']"
+          v-model="c2"
+        >
+          <v-radio
+            value="a"
+            label="The computer picks the same card for you and the other player"
+          >
+          </v-radio>
+          <v-radio
+            value="b"
+            label="The computer picks a different card for you and the other player."
+          >
+          </v-radio>
+          <v-radio
+            value="c"
+            label="The computer makes two random picks: one for you and one for the other player. This card can be the same or a different card."
+          >
+          </v-radio>
+        </v-radio-group>
+        <v-btn
+          color="success"
+          elevation="3"
+          :disabled="!isvalid2"
+          @click="
+            c2clicks++;
+            validateC2();
+          "
+        >
+          Submit
+        </v-btn>
+        <div v-show="c2clicks == 1" style="color:red">
+          {{ warning }}
+        </div>
+      </v-form>
+    </div>
+    <div id="c3" v-show="showC3">
+      <v-form v-model="isvalid3">
+        Let’s say you reported the blue card and the other player reported
+        red:<br />
+        <v-radio-group
+          :rules="[(v) => !!v || 'Please answer this question.']"
+          v-model="c3"
+        >
+          <v-radio
+            value="a"
+            label="You win 1 point. The other player wins nothing."
+          >
+          </v-radio>
+          <v-radio
+            value="b"
+            label="The other player wins 1 point.  You win nothing."
+          >
+          </v-radio>
+          <v-radio
+            value="c"
+            label="You win 1 point. The other player loses 1 point."
+          >
+          </v-radio>
+          <v-radio
+            value="d"
+            label="The other player wins 1 point.  You lose 1 point."
+          >
+          </v-radio>
+        </v-radio-group>
+        <v-btn
+          color="success"
+          elevation="3"
+          :disabled="!isvalid3"
+          @click="
+            c3clicks++;
+            validateC3();
+          "
+        >
+          Submit
+        </v-btn>
+        <div v-show="c3clicks == 1" style="color:red">
+          {{ warning }}
+        </div>
+      </v-form>
+    </div>
+    <div id="c4" v-show="showC4">
+      Well done!<br /><br />
+      Next, you will play three <b>practice trials</b>.<br />
+      After these, the actual game will start. <br /><br />
+      <v-btn
+        color="primary"
+        elevation="3"
+        @click="
+          saveAttempts();
+          $router.push('testtrials');
+        "
+        >Continue</v-btn
+      >
+    </div>
+  </div>
 </template>
-        
+
 <script>
-import { writeResponseData } from "../firebaseConfig"
+import { writeResponseData } from "../firebaseConfig";
 
 export default {
-        data() {
-                return {
-                        c1: '',
-                        c2: '',
-                        c3: '',
-                        c1clicks: 0,
-                        c2clicks: 0,
-                        c3clicks: 0,
-                        showC1: true, 
-                        showC2: false, 
-                        showC3: false,
-                        showC4: false,
-                        isvalid: false,
-                        isvalid2: false,
-                        isvalid3: false,
-                }
-        },
-        methods: {
-                validateC1: function() {
-                        if (this.c1 == 1) {
-                                this.showC1 = false
-                                this.showC2 = true
-                        }
-                        else alert("This answer is incorrect.")
-                },
-                validateC2: function() {
-                        if (this.c2 === 'c') {
-                                this.showC1 = false
-                                this.showC2 = false
-                                this.showC3 = true
-                        }
-                        else alert("This answer is incorrect. Please review the instructions (click on the arrow to go back) and try again.")
-                },
-                validateC3: function() {
-                        if (this.c3 === 'c') {
-                                this.showC1 = false
-                                this.showC2 = false
-                                this.showC3 = false
-                                this.showC4 = true
-                        }
-                        else alert("This answer is incorrect. Please review the instructions (click on the arrow to go back) and try again.")
-                },
-                saveAttempts: function() {
-                        let data = {
-                                c1attempts: this.c1clicks,
-                                c2attempts: this.c2clicks,
-                                c3attempts: this.c3clicks,
-                                c1: this.c1,
-                                c2: this.c2,
-                                c3: this.c3,
-                        }
-                        writeResponseData(this.$uuid, 'comprehension', data)
-                }
-        }
-}
+  data() {
+    return {
+      c1: "",
+      c2: "",
+      c3: "",
+      c1clicks: 0,
+      c2clicks: 0,
+      c3clicks: 0,
+      showC1: true,
+      showC2: false,
+      showC3: false,
+      showC4: false,
+      isvalid: false,
+      isvalid2: false,
+      isvalid3: false,
+      warning:
+        "This answer is incorrect. You have one attempt left. Please review the instructions again before your next attempt. When you use the blue arrows on the side to go back through the instructions and come back here, you will be directed to this question again.",
+    };
+  },
+  methods: {
+    validateC1: function() {
+      if ((this.c1clicks == 2) & (this.c1 == 0)) {
+        window.location.replace("http://www.w3schools.com");
+      } else if (this.c1 == 1) {
+        this.showC1 = false;
+        this.showC2 = true;
+      }
+    },
+    validateC2: function() {
+      if ((this.c2clicks == 2) & (this.c2 !== "c")) {
+        window.location.replace("http://www.w3schools.com");
+      } else if (this.c2 === "c") {
+        this.showC2 = false;
+        this.showC3 = true;
+      }
+    },
+    validateC3: function() {
+      if ((this.c3clicks == 2) & (this.c3 !== "c")) {
+        window.location.replace("http://www.w3schools.com");
+      } else if (this.c3 === "c") {
+        this.showC3 = false;
+        this.showC4 = true;
+      }
+    },
+    saveAttempts: function() {
+      let data = {
+        c1attempts: this.c1clicks,
+        c2attempts: this.c2clicks,
+        c3attempts: this.c3clicks,
+        c1: this.c1,
+        c2: this.c2,
+        c3: this.c3,
+      };
+      writeResponseData(this.$uuid, "comprehension", data);
+    },
+  },
+};
 </script>
-        
--- src/views/Instruction.vue
+++ src/views/Instruction.vue
@@ -1,68 +1,85 @@
 <template>
-<div id="instruct">
-  <swiper class="swiper" :options="swiperOption">
-    <swiper-slide>
-      <div class="instruction">
-        You will play a card game with another particpant.<br />
-        In each trial, you and the other player will be presented with an
-        <b>identical</b> set of five cards. <br>
-        Each card is either red or blue. For
-        example: <br /><br />
-        <div class="red-card-mini" v-for="index in 3" :key="'r1' + index"></div>
-        <div class="blue-card-mini" v-for="index in 2" :key="'b1' + index"></div>
-        <br />
-        The computer then randomly picks a card for you. <b>Only you</b> can see
-        this card.<br /><br />
-        <div class="red-card-mini" v-for="index in 2" :key="'r2' + index"></div>
-        <div
-          class="red-card-mini"
-          style="border:5px solid gold;width:25px;height:43px"
-        ></div>
-        <div class="blue-card-mini" v-for="index in 2" :key="'b2' + index"></div>
-        <br />
-        Your task is to report the card that was picked for you by clicking on
-        it.
-      </div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="instruction">
-        The computer also randomly picks a card for the other player.
-        <br> This card can be the same or different than yours.<br /><br />
+  <div id="instruct">
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide>
+        <div class="instruction">
+          You will play a card game with another particpant.<br />
+          In each trial, you and the other player will be presented with an
+          <b>identical</b> set of five cards. <br />
+          Each card is either red or blue. For example: <br /><br />
+          <div
+            class="red-card-mini"
+            v-for="index in 3"
+            :key="'r1' + index"
+          ></div>
+          <div
+            class="blue-card-mini"
+            v-for="index in 2"
+            :key="'b1' + index"
+          ></div>
+          <br />
+          The computer then randomly picks a card for you. <b>Only you</b> can
+          see this card.<br /><br />
+          <div
+            class="red-card-mini"
+            v-for="index in 2"
+            :key="'r2' + index"
+          ></div>
+          <div
+            class="red-card-mini"
+            style="border:5px solid gold;width:25px;height:43px"
+          ></div>
+          <div
+            class="blue-card-mini"
+            v-for="index in 2"
+            :key="'b2' + index"
+          ></div>
+          <br />
+          Your task is to report the card that was picked for you by clicking on
+          it.
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="instruction">
+          The computer also randomly picks a card for the other player.
+          <br />
+          This card <b>can be the same or different</b> than yours.<br /><br />
 
-        You <b>cannot see</b> what card is picked for the other player.<br><br>
+          You <b>cannot see</b> what card is picked for the other player.<br /><br />
 
-         The other player also reports the card colour that was picked for them.<br />
-        The blue card is the winning card, the red card is the losing card.<br />
-        For example, if you report the blue card and the other player reports
-        the red card:<br /><br />
-        <img src="sswin.png" height="150px" /><br /><br />
-        This means you <b>win</b> 1 point and the other player <b>loses</b> 1
-        point.<br /><br>
-        If both cards have the same colour, no one wins or loses a point.<br />
-      </div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="instruction">
-        For each point you win, you gain £ 0.05.<br />
-        For each point you lose, you lose £ 0.05.<br /><br >
-        Your total bonus payment will <b>not</b> be lower than £ 0.<br /><br />
+          The other player also reports the card colour that was picked for
+          them.<br />
+          The blue card is the winning card, the red card is the losing card.<br />
+          For example, if you report the blue card and the other player reports
+          the red card:<br /><br />
+          <img src="sswin.png" height="150px" /><br /><br />
+          This means you <b>win</b> 1 point and the other player <b>loses</b> 1
+          point.<br /><br />
+          If both cards have the same colour, no one wins or loses a point.<br />
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="instruction">
+          For each point you win, you gain £ 0.05.<br />
+          For each point you lose, you lose £ 0.05.<br /><br />
+          Your total bonus payment will <b>not</b> be lower than £ 0.<br /><br />
 
-        After each trial, you will be asked to rate how honest you think the
-        other player is.<br />
-        Note: The other player also rates your honesty.<br /><br />
-        Next, you are asked to answer a few questions to make sure that you
-        understand the game.
-      </div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="instruction">
-        <ComprehensionCheck />
-      </div>
-    </swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-    <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-  </swiper>
+          After each trial, you will be asked to rate how honest you think the
+          other player is.<br />
+          Note: The other player also rates your honesty.<br /><br />
+          Next, you are asked to answer a few questions to make sure that you
+          understand the game.
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="instruction">
+          <ComprehensionCheck />
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
   </div>
 </template>
 
@@ -99,10 +116,10 @@ export default {
       },
     };
   },
-  mounted () {
-    window.scrollTo(0, 0)
-    this.startTime = Date.now()
-  }
+  mounted() {
+    window.scrollTo(0, 0);
+    this.startTime = Date.now();
+  },
 };
 </script>
 
