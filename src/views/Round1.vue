930d746ca4bd706237edc75e34c34b1cd1cdd43f
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-08-10 15:19:09 +0200

    3 blocks conditional trial setups

---
+++ src/views/Round1.vue
@@ -0,0 +1,205 @@
+<template>
+  <div id="task">
+    <v-progress-linear
+      :value="rounds"
+      color="amber"
+      height="25px"
+      style="margin-top:0; margin-bottom:50px; display:inline-block;"
+      v-if="index < 30"
+      >Trial {{ trialIndex + 1 }} of {{ trials.length }}</v-progress-linear
+    >
+    <div id="taskloop" v-for="(trial, index) in trials" :key="index">
+      <div v-show="index === trialIndex">
+        <div id="cards" v-show="showCards">
+          <Cards
+            :index="index"
+            :n_red="trial.n_red"
+            :trial="false"
+            :timeTilPick="1100"
+            :report_duration="duration"
+            :durationx="3"
+            @card_pick="savePick"
+            @card_pick_colour="savePickColour"
+            @done="toReport"
+          />
+        </div>
+        <div id="report" v-show="showReport">
+          <Report
+            :index="index"
+            :outcome="trial.outcome"
+            :timeTilOutcome="1000"
+            @report="saveResponse"
+            @rt_report="saveRTreport"
+            @honesty_rating="saveHonestyRating"
+            @rt_honesty="saveRThonesty"
+            @catch_rating="saveCatchRating"
+            @rt_catch="saveRTcatch"
+            @result="saveResult"
+            @ratingdone="next"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-show="this.trialIndex === trials.length" style="text-align:left;">
+      <b>You finished the first game.</b> <br /><br />
+      {{ endText() }}
+      <br /><br />
+      <v-btn
+        color="primary"
+        elevation="3"
+        @click="
+          saveAll();
+          $router.push('round2');
+        "
+        ><b>Continue</b></v-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import bs11 from "@/assets/p2b_s11.json";
+import bs02 from "@/assets/p2b_s02.json";
+import as31 from "@/assets/p2a_s31.json";
+import r0 from "@/assets/r0.json";
+
+import Cards from "@/components/Cards.vue";
+import Report from "@/components/Report.vue";
+
+import { writeResponseData } from "../firebaseConfig";
+
+export default {
+  components: {
+    Cards,
+    Report,
+  },
+  data() {
+    return {
+      index: 0,
+      trialIndex: 0,
+      duration: 2000,
+      showCards: true,
+      showReport: false,
+      start: 0,
+      randomPick: [],
+      randomPickColour: [],
+      reportColour: [],
+      RTreport: [],
+      honestyRating: [],
+      RThonesty: [],
+      catchRating: [],
+      RTcatch: [],
+      results: [],
+    };
+  },
+  computed: {
+    rounds: function() {
+      return ((this.trialIndex + 1) * 100) / this.trials.length;
+    },
+    wins: function() {
+      return this.results.filter((i) => i === "win").length;
+    },
+    losses: function() {
+      return this.results.filter((i) => i === "loss").length;
+    },
+    points: function() {
+      return this.wins - this.losses;
+    },
+    bonus: function() {
+      if (this.points <= 0) {
+        return 0;
+      }
+      return (0.05 * this.points).toFixed(2);
+    },
+  },
+  methods: {
+    toReport: function() {
+      this.showCards = false;
+      this.showReport = true;
+      this.start = Date.now();
+    },
+    savePick: function(v) {
+      this.randomPick.push(v);
+      // console.log("random card pick:", this.randomPick.slice(0,trials.length))
+    },
+    savePickColour: function(v) {
+      this.randomPickColour.push(v);
+      // console.log("random pick colour:", this.randomPickColour.slice(0,trials.length))
+    },
+    saveResponse: function(v) {
+      this.reportColour.push(v);
+      // console.log("reported colour", this.reportColour)
+    },
+    saveRTreport: function(v) {
+      this.RTreport.push(v);
+      // console.log("PARENT - RT report", this.RThonesty)
+    },
+    saveHonestyRating: function(v) {
+      this.honestyRating.push(v);
+      // console.log("PARENT - honesty rating", this.honestyRating)
+    },
+    saveRThonesty: function(v) {
+      this.RThonesty.push(v);
+      // console.log("PARENT - RT honesty", this.RThonesty)
+    },
+    saveCatchRating: function(v) {
+      this.catchRating.push(v);
+      // console.log("PARENT - catch rating", this.catchRating)
+    },
+    saveRTcatch: function(v) {
+      this.RTcatch.push(v);
+      // console.log("PARENT - RT catch", this.RTcatch)
+    },
+    saveResult: function(v) {
+      this.results.push(v);
+    },
+    next: function() {
+      this.duration = Date.now() - this.start;
+      this.index++;
+      this.trialIndex++;
+      this.showCards = true;
+      this.showReport = false;
+    },
+    endText: function() {
+      if (this.$condition > 0) {
+        return "The next game will be with another participant from a previous study.";
+      } else {
+        return "Please click 'Continue' below to start the next game.";
+      }
+    },
+    saveAll: function() {
+      let responses = {
+        randomPick: this.randomPick.slice(0, this.trials.length),
+        randomPickColour: this.randomPickColour.slice(0, this.trials.length),
+        reportColour: this.reportColour,
+        RTreport: this.RTreport,
+        honestyRating: this.honestyRating,
+        RThonesty: this.RThonesty,
+        catchRating: this.catchRating,
+        RTcatch: this.RTcatch,
+        results: this.results,
+        bonusAmountGBP: this.bonus,
+        finishTime: performance.now()
+      };
+      writeResponseData(this.$uuid, "game_block1", responses);
+    },
+  },
+  created() {
+    this.trials =
+      (this.$condition === 11) | (this.$condition === 12)
+        ? bs11
+        : (this.$condition === 21) | (this.$condition === 22)
+        ? as31
+        : (this.$condition === 31) | (this.$condition === 32)
+        ? bs02
+        : r0;
+  },
+};
+</script>
+
+<style scoped>
+#task {
+  text-align: center;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/Round3.vue
@@ -0,0 +1,206 @@
+<template>
+  <div id="task">
+    <v-progress-linear
+      :value="rounds"
+      color="amber"
+      height="25px"
+      style="margin-top:0; margin-bottom:50px; display:inline-block;"
+      v-if="index < 30"
+      >Trial {{ trialIndex + 1 }} of {{ trials.length }}</v-progress-linear
+    >
+    <div id="taskloop" v-for="(trial, index) in trials" :key="index">
+      <div v-show="index === trialIndex">
+        <div id="cards" v-show="showCards">
+          <Cards
+            :index="index"
+            :n_red="trial.n_red"
+            :trial="false"
+            :timeTilPick="1100"
+            :report_duration="duration"
+            :durationx="3"
+            @card_pick="savePick"
+            @card_pick_colour="savePickColour"
+            @done="toReport"
+          />
+        </div>
+        <div id="report" v-show="showReport">
+          <Report
+            :index="index"
+            :outcome="trial.outcome"
+            :timeTilOutcome="1000"
+            @report="saveResponse"
+            @rt_report="saveRTreport"
+            @honesty_rating="saveHonestyRating"
+            @rt_honesty="saveRThonesty"
+            @catch_rating="saveCatchRating"
+            @rt_catch="saveRTcatch"
+            @result="saveResult"
+            @ratingdone="next"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-show="this.trialIndex === trials.length" style="text-align:left;">
+      <b>You finished the last game.</b> <br /><br />
+
+      Next is the last part of this study, where you will be asked questions about yourself.<br />
+      Please click "Continue" below to proceed.<br /><br />
+      <v-btn
+        color="primary"
+        elevation="3"
+        @click="
+          saveAll();
+          $router.push('survey');
+        "
+        ><b>Continue</b></v-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import bs11 from "@/assets/p2b_s11.json";
+import bs02 from "@/assets/p2b_s02.json";
+import as31 from "@/assets/p2a_s31.json";
+import r2 from "@/assets/r2.json";
+
+import Cards from "@/components/Cards.vue";
+import Report from "@/components/Report.vue";
+
+import { writeResponseData } from "../firebaseConfig";
+
+export default {
+  components: {
+    Cards,
+    Report,
+  },
+  data() {
+    return {
+      index: 0,
+      trialIndex: 0,
+      duration: 2000,
+      showCards: true,
+      showReport: false,
+      start: 0,
+      randomPick: [],
+      randomPickColour: [],
+      reportColour: [],
+      RTreport: [],
+      honestyRating: [],
+      RThonesty: [],
+      catchRating: [],
+      RTcatch: [],
+      results: [],
+    };
+  },
+  computed: {
+    rounds: function() {
+      return ((this.trialIndex + 1) * 100) / this.trials.length;
+    },
+    wins: function() {
+      return this.results.filter((i) => i === "win").length;
+    },
+    losses: function() {
+      return this.results.filter((i) => i === "loss").length;
+    },
+    points: function() {
+      return this.wins - this.losses;
+    },
+    bonus: function() {
+      if (this.points <= 0) {
+        return 0;
+      }
+      return (0.05 * this.points).toFixed(2);
+    },
+  },
+  methods: {
+    toReport: function() {
+      this.showCards = false;
+      this.showReport = true;
+      this.start = Date.now();
+    },
+    savePick: function(v) {
+      this.randomPick.push(v);
+      // console.log("random card pick:", this.randomPick.slice(0,trials.length))
+    },
+    savePickColour: function(v) {
+      this.randomPickColour.push(v);
+      // console.log("random pick colour:", this.randomPickColour.slice(0,trials.length))
+    },
+    saveResponse: function(v) {
+      this.reportColour.push(v);
+      // console.log("reported colour", this.reportColour)
+    },
+    saveRTreport: function(v) {
+      this.RTreport.push(v);
+      // console.log("PARENT - RT report", this.RThonesty)
+    },
+    saveHonestyRating: function(v) {
+      this.honestyRating.push(v);
+      // console.log("PARENT - honesty rating", this.honestyRating)
+    },
+    saveRThonesty: function(v) {
+      this.RThonesty.push(v);
+      // console.log("PARENT - RT honesty", this.RThonesty)
+    },
+    saveCatchRating: function(v) {
+      this.catchRating.push(v);
+      // console.log("PARENT - catch rating", this.catchRating)
+    },
+    saveRTcatch: function(v) {
+      this.RTcatch.push(v);
+      // console.log("PARENT - RT catch", this.RTcatch)
+    },
+    saveResult: function(v) {
+      this.results.push(v);
+    },
+    next: function() {
+      this.duration = Date.now() - this.start;
+      this.index++;
+      this.trialIndex++;
+      this.showCards = true;
+      this.showReport = false;
+    },
+    endText: function() {
+      if (this.$condition > 0) {
+        return "The next game is the last one, again with another participant from a previous study.";
+      } else {
+        return "Please click 'Continue' below to start the third and last game.";
+      }
+    },
+    saveAll: function() {
+      let responses = {
+        randomPick: this.randomPick.slice(0, this.trials.length),
+        randomPickColour: this.randomPickColour.slice(0, this.trials.length),
+        reportColour: this.reportColour,
+        RTreport: this.RTreport,
+        honestyRating: this.honestyRating,
+        RThonesty: this.RThonesty,
+        catchRating: this.catchRating,
+        RTcatch: this.RTcatch,
+        results: this.results,
+        bonusAmountGBP: this.bonus,
+        finishTime: performance.now()
+      };
+      writeResponseData(this.$uuid, "game_block3", responses);
+    },
+  },
+  created() {
+    this.trials =
+      (this.$condition === 22) | (this.$condition === 32)
+        ? bs11
+        : (this.$condition === 12) | (this.$condition === 31)
+        ? as31
+        : (this.$condition === 11) | (this.$condition === 21)
+        ? bs02
+        : r2;
+  },
+};
+</script>
+
+<style scoped>
+#task {
+  text-align: center;
+  overflow: hidden;
+}
+</style>
