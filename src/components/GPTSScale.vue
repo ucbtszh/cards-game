dfd0d2768cdcacf7c3188a07a6be0fcdfea114b0
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-10-06 13:56:17 +0200

    paranoia R-GPTS questionnaire

---
+++ src/components/GPTSScale.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="likert">
+    <v-radio-group :rules="[(v) => !!v || 'This field is required']">
+      <div class="statement">{{ name }}</div>
+      <br />
+      <v-radio
+        label="Not at all"
+        value="1"
+        aria-required="true"
+        required
+        :name="name"
+        @click="
+          $emit('response_clicked', [
+            name,
+            Date.now(),
+            1
+          ])
+        "
+      ></v-radio>
+      <v-radio
+        label=""
+        value="2"
+        aria-required="true"
+        required
+        :name="name"
+        @click="
+          $emit('response_clicked', [
+            name,
+            Date.now(),
+            2
+          ])
+        "
+      ></v-radio>
+      <v-radio
+        label=""
+        value="3"
+        aria-required="true"
+        required
+        :name="name"
+        @click="
+          $emit('response_clicked', [
+            name,
+            Date.now(),
+            3
+          ])
+        "
+      ></v-radio>
+      <v-radio
+        label=""
+        value="4"
+        aria-required="true"
+        required
+        :name="name"
+        @click="
+          $emit('response_clicked', [
+            name,
+            Date.now(),
+            4
+          ])
+        "
+      ></v-radio>
+      <v-radio
+        label="Totally"
+        value="5"
+        aria-required="true"
+        required
+        :name="name"
+        @click="
+          $emit('response_clicked', [
+            name,
+            Date.now(),
+            5
+          ])
+        "
+      ></v-radio>
+    </v-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+};
+</script>
+
+<style scoped>
+.statement {
+  font-weight: bold;
+  font-size: 26px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/components/RGPTS.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="RGPTS">
+    
+    Please indicate how much you agree with each of the below statements.<br />
+    <v-form v-model="isValid">
+      <GPTSScale
+        v-for="(q, index) in items"
+        :key="index"
+        :name="q.item"
+        @response_clicked="trackResponse"
+      />
+      <v-btn
+        color="primary"
+        elevation="3"
+        :disabled="!isValid"
+        @click="$emit('RGPTS_done'); saveResponses()"
+      >
+        NEXT
+      </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import items from "@/assets/RGPTS.json";
+
+import GPTSScale from "./GPTSScale.vue";
+
+import { writeResponseData } from "../firebaseConfig";
+
+export default {
+  components: {
+    GPTSScale
+  },
+  data() {
+    return {
+      items: items,
+      isValid: true,
+      RGPTSResponse: {},
+    };
+  },
+  methods: {
+    trackResponse: function(response) {
+      this.RGPTSResponse[response[0]] = response[2]
+    },
+    saveResponses: function() {
+        // console.log("RGPTS WRITE DATA TO DB", this.EQResponse)
+      writeResponseData(this.$uuid, 'RGPTS', this.RGPTSResponse)
+    }
+  },
+  mounted() {
+    window.scrollTo(0, 0);
+  },
+};
+</script>
+
+<style scoped>
+#RGPTS {
+  margin-top: 5%;
+}
+</style>
