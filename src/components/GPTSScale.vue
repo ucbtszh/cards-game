8e29575b8954a7290199f60f08ad513eee1d5278
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-10-11 10:46:17 +0200

    validate rgpts with radio scale

--- src/components/GPTSScale.vue
+++ src/components/GPTSScale.vue
@@ -1,24 +1,74 @@
 <template>
   <div class="likert">
     <div class="statement">{{ name }}</div>
-    <v-card-text>
-      <v-slider
-        v-model="response"
-        :min="1"
-        :max="5"
-        @click="$emit('response_clicked', [name, Date.now(), response])"
-        :rules="[!!response || 'Please answer this question']"
-        step="1"
-        ticks="always"
-        tick-size="10"
-        ><template v-slot:prepend>
-          <h4>Not at all</h4>
-        </template>
-        <template v-slot:append>
-          <h4>Totally</h4>
-        </template>
-      </v-slider>
-    </v-card-text>
+    <v-radio-group
+      :rules="[(v) => !!v || 'Please answer this question']"
+      v-model="response"
+    >
+      <v-layout row wrap>
+        <v-flex column>
+          <v-radio
+            value="0"
+            aria-required="true"
+            required
+            :name="name"
+            @click="$emit('response_clicked', [name, Date.now(), 0])"
+          >
+          </v-radio>
+          <p>0</p>
+        </v-flex>
+        <v-flex column>
+          <v-radio
+            value="1"
+            aria-required="true"
+            required
+            :name="name"
+            @click="$emit('response_clicked', [name, Date.now(), 1])"
+          >
+          </v-radio>
+          <p>1</p>
+        </v-flex>
+        <v-flex column>
+          <v-radio
+            value="2"
+            aria-required="true"
+            required
+            :name="name"
+            @click="$emit('response_clicked', [name, Date.now(), 2])"
+          >
+          </v-radio>
+          <p>2</p>
+        </v-flex>
+        <v-flex column>
+          <v-radio
+            value="3"
+            aria-required="true"
+            required
+            :name="name"
+            @click="$emit('response_clicked', [name, Date.now(), 3])"
+          >
+          </v-radio>
+          <p>3</p>
+        </v-flex>
+        <v-flex column>
+          <v-radio
+            value="4"
+            aria-required="true"
+            required
+            :name="name"
+            @click="$emit('response_clicked', [name, Date.now(), 4])"
+          >
+          </v-radio>
+          <p>4</p>
+        </v-flex>
+      </v-layout>
+      <template v-slot:prepend>
+        <h4>Not at all</h4>
+      </template>
+      <template v-slot:append>
+        <h4>Totally</h4>
+      </template>
+    </v-radio-group>
   </div>
 </template>
 
@@ -40,5 +90,9 @@ export default {
   font-size: 26px;
   margin-top: 75px;
   margin-bottom: 25px;
+}
+
+p {
+  font-size: 20px;
 }
 </style>
