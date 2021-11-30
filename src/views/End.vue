98bf8da7255a97b3e0a0bfed64d00f070e82db84
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-11-30 11:08:44 +0000

    slight change in formulation

--- src/views/End.vue
+++ src/views/End.vue
@@ -2,7 +2,7 @@
     <div id="end">
         In this study, you were playing with actual responses from participants from previous studies.<br /><br />
 
-        <b>Thank you very much for participating in this study.</b> <br />
+        <b>Thank you very much for your participation.</b> <br />
         We greatly appreciate your time and effort in doing so, as it allows us to continue academic research during these challenging times.<br><br>
         Your participation will be logged automatically once you click the below button:<br /><br />
         <v-btn href="https://app.prolific.co/submissions/complete?cc=7BF55447" color="primary" elevation="3"
